e095e85e2e2f4f86aeef23cdf64467e7566a1fb9
Emre Akay <emre.akayy@hotmail.com>
2024-07-18 20:35:30 +0300

    The 2nd code was written for choice random numbers beetween with 1-80.

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ for i in range(3):
     numara = int(input(f"{i+1}. Sabit Numarayı Girin: "))
     sabit_numaralar.append(numara)
 
+# Sabit numaralar haricinde; 1 ile 80 sayıları arasında, 7 rastgele sayı seçimi
+rastgele_numaralar = random.sample([i for i in range(1, 80) if i not in sabit_numaralar], 7)
+
