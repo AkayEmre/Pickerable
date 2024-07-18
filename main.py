b9ca796d50c475aa0000cfb43f61848dfb2b076f
Emre Akay <emre.akayy@hotmail.com>
2024-07-18 20:33:15 +0300

    The 1st code was written to get 3 fixed numbers from user.

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 import random
+
+# Kullanıcıdan sabit numaraları al
+sabit_numaralar = []
+for i in range(3):
+    numara = int(input(f"{i+1}. Sabit Numarayı Girin: "))
+    sabit_numaralar.append(numara)
+
